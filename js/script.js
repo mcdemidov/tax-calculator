abfeebf6ec39b4826eb11b21134ea89764035b5f
msdemidov <androjob@gmail.com>
2022-10-13 12:35:54 +0300

    Add function getCalc

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const navigationLinks = document.querySelectorAll('.navigation__link');
+const calcElems = document.querySelectorAll('.calc');
+
+//for (let i = 0; i < navigationLinks.length; i++) {
+
+//  console.log(i, navigationLinks[i]);
+//}
+
+const getCalc = el => {
+  calcElems.forEach(item => {
+    if (item.dataset.tax === el) {
+      item.classList.add('calc_active');
+    } else {
+      item.classList.remove('calc_active');
+    }
+  });
+};
+
+navigationLinks.forEach(el => {
+  el.addEventListener('click', e => {
+    let link = e.target;
+    clearActiveLink();
+    link.classList.add('navigation__link_active');
+
+    getCalc(link.dataset.tax);
+  });
+});
+
+console.log(navigationLinks);
